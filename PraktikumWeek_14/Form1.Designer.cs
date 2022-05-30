9dd41d30c64119e1a7a27cfa1e02761204f45224
Yves King Chandra <ychandra02@student.ciputra.ac.id>
2022-05-30 11:29:40 +0700

    Add project files.

---
+++ PraktikumWeek_14/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace PraktikumWeek_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DGV5Match = new System.Windows.Forms.DataGridView();
+            this.lblTeamName = new System.Windows.Forms.Label();
+            this.lblManager = new System.Windows.Forms.Label();
+            this.lblStadium = new System.Windows.Forms.Label();
+            this.lblTopScore = new System.Windows.Forms.Label();
+            this.lblWorstDisc = new System.Windows.Forms.Label();
+            this.lblJumlahGoal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV5Match)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(12, 25);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 43);
+            this.btnFirst.TabIndex = 0;
+            this.btnFirst.Text = "<<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(111, 25);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 41);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(212, 25);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 41);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(320, 25);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 41);
+            this.btnLast.TabIndex = 3;
+            this.btnLast.Text = ">>";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Team Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Manager :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Stadium :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Top Scorer :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Worst Discipline :";
+            // 
+            // DGV5Match
+            // 
+            this.DGV5Match.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV5Match.Location = new System.Drawing.Point(16, 256);
+            this.DGV5Match.Name = "DGV5Match";
+            this.DGV5Match.Size = new System.Drawing.Size(461, 182);
+            this.DGV5Match.TabIndex = 9;
+            // 
+            // lblTeamName
+            // 
+            this.lblTeamName.AutoSize = true;
+            this.lblTeamName.Location = new System.Drawing.Point(112, 88);
+            this.lblTeamName.Name = "lblTeamName";
+            this.lblTeamName.Size = new System.Drawing.Size(35, 13);
+            this.lblTeamName.TabIndex = 10;
+            this.lblTeamName.Text = "label6";
+            // 
+            // lblManager
+            // 
+            this.lblManager.AutoSize = true;
+            this.lblManager.Location = new System.Drawing.Point(112, 120);
+            this.lblManager.Name = "lblManager";
+            this.lblManager.Size = new System.Drawing.Size(35, 13);
+            this.lblManager.TabIndex = 11;
+            this.lblManager.Text = "label7";
+            // 
+            // lblStadium
+            // 
+            this.lblStadium.AutoSize = true;
+            this.lblStadium.Location = new System.Drawing.Point(112, 152);
+            this.lblStadium.Name = "lblStadium";
+            this.lblStadium.Size = new System.Drawing.Size(35, 13);
+            this.lblStadium.TabIndex = 12;
+            this.lblStadium.Text = "label8";
+            // 
+            // lblTopScore
+            // 
+            this.lblTopScore.AutoSize = true;
+            this.lblTopScore.Location = new System.Drawing.Point(112, 184);
+            this.lblTopScore.Name = "lblTopScore";
+            this.lblTopScore.Size = new System.Drawing.Size(35, 13);
+            this.lblTopScore.TabIndex = 13;
+            this.lblTopScore.Text = "label9";
+            // 
+            // lblWorstDisc
+            // 
+            this.lblWorstDisc.AutoSize = true;
+            this.lblWorstDisc.Location = new System.Drawing.Point(109, 216);
+            this.lblWorstDisc.Name = "lblWorstDisc";
+            this.lblWorstDisc.Size = new System.Drawing.Size(41, 13);
+            this.lblWorstDisc.TabIndex = 14;
+            this.lblWorstDisc.Text = "label10";
+            // 
+            // lblJumlahGoal
+            // 
+            this.lblJumlahGoal.AutoSize = true;
+            this.lblJumlahGoal.Location = new System.Drawing.Point(209, 184);
+            this.lblJumlahGoal.Name = "lblJumlahGoal";
+            this.lblJumlahGoal.Size = new System.Drawing.Size(35, 13);
+            this.lblJumlahGoal.TabIndex = 15;
+            this.lblJumlahGoal.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 450);
+            this.Controls.Add(this.lblJumlahGoal);
+            this.Controls.Add(this.lblWorstDisc);
+            this.Controls.Add(this.lblTopScore);
+            this.Controls.Add(this.lblStadium);
+            this.Controls.Add(this.lblManager);
+            this.Controls.Add(this.lblTeamName);
+            this.Controls.Add(this.DGV5Match);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV5Match)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView DGV5Match;
+        private System.Windows.Forms.Label lblTeamName;
+        private System.Windows.Forms.Label lblManager;
+        private System.Windows.Forms.Label lblStadium;
+        private System.Windows.Forms.Label lblTopScore;
+        private System.Windows.Forms.Label lblWorstDisc;
+        private System.Windows.Forms.Label lblJumlahGoal;
+    }
+}
+
